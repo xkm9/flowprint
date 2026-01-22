ac0f86e9a71ccdef03f72ac3f1d568a31a84e221
cr1tii <lostmemories531@gmail.com>
2026-01-22 03:54:53 +0200

    fixing the space bug again

--- lib/patterns.py
+++ lib/patterns.py
@@ -3,8 +3,8 @@ built_in_patterns = {
         "class": """    # a class statement\n    print('class statement')\n""",
         "def" : """    # a def statement\n    print('def statement')\n""",
         "import": """# an import statement\nprint('import statement')\n""",
-        "for" : """    # for loop statement\n    print('for loop statement')\n""",
         "while": """    # here is while loop statement\n    print('while loop statement')\n""",
+        "for": """    # a for loop statement\n    print('for loop statement')\n""",
         "if": """    #a if condition statement\n    print('if statement')\n""",
         "elif" : """    # a elif condition statement\n    print('elif statement)\n"""
 
