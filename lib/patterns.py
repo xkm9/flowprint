27a1c620fc58f7ab537c29a3ca616e5118bf01ca
cr1tii <lostmemories531@gmail.com>
2026-01-24 09:41:40 +0200

    im done with these tags, patterns file needs a whole extension with 200+ patterns

--- lib/patterns.py
+++ lib/patterns.py
@@ -1,11 +1,11 @@
 built_in_patterns = {
 
-        "class": """    \n# a class statement\n    print('class statement')\n""",
-        "def" : """   \n # a def statement\n    print('def statement')\n""",
-        "import": """\n# an import statement\nprint('import statement')\n""",
-        "while": """    \n# here is while loop statement\n    print('while loop statement')\n""",
-        "if": """    \n#a if condition statement\n    print('if statement')\n""",
-        "elif" : """    \n# a elif condition statement\n    print('elif statement')\n"""
+        "class": """   print('class statement')\n""",
+        "def" : """    print('def statement')\n""",
+        "import": """print('import statement')\n""",
+        "while": """    print('while loop statement')\n""",
+        "if": """    print('if statement')\n""",
+        "elif" : """    print('elif statement')\n"""
 
 
         }
